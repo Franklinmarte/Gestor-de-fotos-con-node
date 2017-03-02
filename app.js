80b7938bd26e5dfd8096e143902d1b219f3d21a2
Franklinmarte <franklin.a.marte@gmail.com>
2017-03-02 16:27:54 -0600

    add styles and sessions

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var express =require("express");
 var bodyParser = require("body-parser");
 var User = require("./models/user").User;
+var session = require("express-session");
+var session_middlewares = require("./middlewares/session");
+var router_app = require("./router_app");
 var app = express();
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(session({
+	secret: "8092302164",
+	resave: false,
+	saveUninitialized: false
+}));
+
 app.set("view engine", "jade");
 app.get("/",function(req, res){
+	console.log(req.session.user_id);
 	res.render("index");
 })
+app.get("/signup",function(req, res){
+	res.render("signup");
+});
 app.get("/login",function(req, res){
 	res.render("login");
 })
@@ -18,11 +31,22 @@ app.post("/users",function(req, res){
 						 password: req.body.pass,
 						 password_confirmation: req.body.pass_confirmation,
 						 username: req.body.username});
-	user.save(function(err){
-		if (err) {
-			console.log(String(err))
-		}
-		res.send("recibimos");
+	user.save().then(function(us){
+		res.send("Guardamos al usuario correctamente")
+	},function(err){
+		console.log(String(err));
+		res.send("No pudimos guardar al usuario debido a")
+	})
+});
+app.post("/sessions",function(req, res){
+	User.findOne({email:req.body.email, password: req.body.pass},function(err,user){	
+		
+		req.session.user_id = user._id;
+		res.redirect("/app")
+
 	});
-})
+});
+app.use("/app",session_middlewares);
+app.use("/app",router_app);
+
 app.listen(8080);
