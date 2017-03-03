d605812ad8335d3b3c83b33b91dffe4ff53f46c1
Franklinmarte <franklinmarte@cloudnine.com.do>
2017-03-02 18:04:46 -0600

    save imagen

---
+++ models/imagenes.js
@@ -0,0 +1,9 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var img_shema = new Schema({
+	title: {type:String , requerid:true}
+});
+var Imagen = mongoose.model("Imagen", img_shema);
+
+module.exports = Imagen;
--- router_app.js
+++ router_app.js
@@ -1,10 +1,51 @@
 var express =require("express");
-
+var Imagen = require("./models/imagenes");
 
 var router =express.Router();
 
 router.get("/", function(req, res){
 	res.render("app/home");
 });
+
+router.get("/imagenes/new", function(req, res){
+	res.render("app/imagenes/new");
+});
+router.get("/imagenes/:id/edit", function(req, res){
+	
+});
+router.route("/imagenes/:id")
+ .get(function(req,res){
+	Imagen.findById(req.params.id,function(err,imagen){
+		res.render("app/imagenes/show",{imagen:imagen });
+	})
+	
+})
+.put(function(req,res){
+
+})
+.delete(function(req,res){
+
+});
+
+router.route("/imagenes")
+.get(function(req,res){
+
+})
+.post(function(req,res){
+	var data = {
+		title: req.body.title
+	};
+	var imagen = new Imagen(data);
+	imagen.save(function(err){
+		if (!err) {
+			res.redirect("/app/imagenes/"+imagen._id);
+		}else
+		{
+			console.log(String(err))
+		}
+	})
+});
+
+
 
 module.exports = router;
